fba13920b583b59a8612ee5a2587389999f69bcd
Amit Pal <amitpal16181@gmail.com>
2023-04-09 16:13:38 +0530

    Minor Changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-dark bg-success navbar-expand-sm">
         <div class="container">
-            <router-link to="/" class="navbar-brand"><i class="fa fa-mobile"></i>Vue Contact Manager</router-link>
+            <router-link to="/" class="navbar-brand"><i class="fa fa-mobile"></i>Contact Manager</router-link>
         </div>
     </nav>
 </template>
